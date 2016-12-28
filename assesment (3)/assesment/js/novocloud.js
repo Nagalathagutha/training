ef5012a937867d46b0e1c4a32282dd61c43c2690
Nagalathagutha <nagalatha777gutha@gmail.com>
2016-12-28 18:35:54 +0530

    asessment

--- assesment (3)/assesment/js/novocloud.js
+++ assesment (3)/assesment/js/novocloud.js
@@ -23,40 +23,28 @@ app.controller('novoCloudCntrl',function($scope, novoService) {
 
 
      });
-    
-    /* novoService.updates('js/graph.json').then(function(graphdata) {
-         $scope.graphdetails = graphdata;
+    novoService.updates('js/graph.json').then(function(graphdata) {
+         $scope.graphdetails = graphdata.Data;
         console.log($scope.graphdetails);
         displayGraph($scope.graphdetails);
-
-     });*/
-     var displayGraph=function() {
-		// console.log(graphdetails)
+     });
+    var displayGraph=function(graphdetails) {
+		 console.log(graphdetails)
       var chart = new CanvasJS.Chart("chartContainer",
       {
-
         animationEnabled: true,
-      legend:{
-       verticalAlign: "bottom",
-       horizontalAlign: "center"
-     },
+          data: [
+            { 
 
-        data: [
-        {       
-          type: "pie",
-         
-          toolTipContent: "{name}: <strong>${y}</strong>",
-              indexLabel: "{name} ${y}",
-              dataPoints: [
-                    {  y: 46615.06, name: "Harvest Cost", exploded: true},
-                    {  y: 2740.00, name: "Transport Cost" },
-                    { y: 3519.75, name: "Cooling Cost" },
-                    {  y: 5658.75, name: "Packing Cost"},
-                    {  y: 20576.00, name: "Growing Cost" }
-                      ]
-                    }
-       ]
+              dataPoints: graphdetails,      
+              type: "pie",
+               
+               indexLabel: "{indexlabel} ${y}"
+                    
+            }
+          ]
       });
       chart.render();
-    }();
+    }
+
 });
