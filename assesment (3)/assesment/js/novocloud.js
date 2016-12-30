1d05b333809b3b6d2ebcbae702e21a89689688fc
Nagalathagutha <nagalatha777gutha@gmail.com>
2016-12-30 12:23:53 +0530

    asessment

--- assesment (3)/assesment/js/novocloud.js
+++ assesment (3)/assesment/js/novocloud.js
@@ -6,7 +6,7 @@ app.directive('novoCloudDirective', function() {
     }
 
 });
-           
+
 app.service('novoService', function($http, $q) {
     this.updates = function(url) {
         var def = $q.defer();
@@ -16,35 +16,32 @@ app.service('novoService', function($http, $q) {
 
     }
 });
-app.controller('novoCloudCntrl',function($scope, novoService) {
-     novoService.updates('js/novocloud.json').then(function(data) {
-         $scope.getData = data;
+app.controller('novoCloudCntrl', function($scope, novoService) {
+    novoService.updates('js/novocloud.json').then(function(data) {
+        $scope.getData = data;
         console.log($scope.getData);
 
 
-     });
+    });
     novoService.updates('js/graph.json').then(function(graphdata) {
-         $scope.graphdetails = graphdata.Data;
+        $scope.graphdetails = graphdata.Data;
         console.log($scope.graphdetails);
         displayGraph($scope.graphdetails);
-     });
-    var displayGraph=function(graphdetails) {
-		 console.log(graphdetails)
-      var chart = new CanvasJS.Chart("chartContainer",
-      {
-        animationEnabled: true,
-          data: [
-            { 
+    });
+    var displayGraph = function(graphdetails) {
+        console.log(graphdetails)
+        var chart = new CanvasJS.Chart("chartContainer", {
+            animationEnabled: true,
+            data: [{
 
-              dataPoints: graphdetails,      
-              type: "pie",
-               
-               indexLabel: "{indexlabel} ${y}"
-                    
-            }
-          ]
-      });
-      chart.render();
+                dataPoints: graphdetails,
+                type: "pie",
+
+                indexLabel: "{indexlabel} ${y}"
+
+            }]
+        });
+        chart.render();
     }
 
 });
