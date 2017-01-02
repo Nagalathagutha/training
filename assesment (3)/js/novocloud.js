2ec0677fb68660b2230e79d54bbb0bfad47dca8c
Nagalathagutha <nagalatha777gutha@gmail.com>
2017-01-02 17:50:45 +0530

    asesment with side navbar

---
+++ assesment (3)/js/novocloud.js
@@ -0,0 +1,56 @@
+var app = angular.module('novoCloudApp', ["ui.router"]);
+app.directive('novoCloudDirective', function() {
+    return {
+        restrict: 'E',
+        templateUrl: './html/navbar.html'
+    }
+
+});
+
+app.service('novoService', function($http, $q) {
+    this.updates = function(url) {
+        var def = $q.defer();
+        $http.get(url).success(def.resolve)
+            .error(def.reject);
+        return def.promise;
+
+    }
+});
+app.controller('novoCloudCntrl', function($scope, novoService) {
+    novoService.updates('js/novocloud.json').then(function(data) {
+        $scope.getData = data;
+        console.log($scope.getData);
+
+
+    });
+    novoService.updates('js/graph.json').then(function(graphdata) {
+        $scope.graphdetails = graphdata.Data;
+        console.log($scope.graphdetails);
+        displayGraph($scope.graphdetails);
+    });
+    var displayGraph = function(graphdetails) {
+        console.log(graphdetails)
+        var chart = new CanvasJS.Chart("chartContainer", {
+            animationEnabled: true,
+            data: [{
+
+                dataPoints: graphdetails,
+                type: "pie",
+
+                indexLabel: "{indexlabel} ${y}"
+
+            }]
+        });
+        chart.render();
+    }
+   
+    $scope.sidenavbar=function(){ 
+    $scope.showme=true;     
+    }
+   
+    $scope.sidenavbarcollapse=function(){ 
+    $scope.showme=false;     
+    }
+
+
+});
